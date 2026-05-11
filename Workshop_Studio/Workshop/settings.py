65a35c06c63bd26194e76a2c55c68ecc2c39d63f
Yara Ayman <yaramhran2006@gmail.com>
2026-05-11 12:49:47 +0300

    yara

--- Workshop_Studio/Workshop/settings.py
+++ Workshop_Studio/Workshop/settings.py
@@ -167,3 +167,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SESSION_ENGINE = 'django.contrib.sessions.backends.file'
